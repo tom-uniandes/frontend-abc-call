fc6400b24dd04ea4e92862c2f478eb22bbbc36dd
santiagomd11 <your_email@example.com>
2024-10-12 10:20:09 -0500

    Adding code for manage-plan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ManagePlanComponent } from './clients/manage-plan/manage-plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, 
+  { path: 'clients/manage-plan', component: ManagePlanComponent,  pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }  // Wildcard route for a 404 page
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
