06840e03dd7d1973ac1309f788750bd9e2081775
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-10-20 20:56:40 -0500

    Add register

---
+++ src/app/register/register.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterClientComponent } from './register-client/register-client.component';
+
+
+
+@NgModule({
+  declarations: [
+    RegisterClientComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegisterModule { }
