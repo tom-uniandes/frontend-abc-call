7ea09afab82369bfaf866593703db954f916f93c
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2024-10-27 22:28:40 -0500

    Servicio para graficar analitica

--- src/app/analitica/services/analitica.service.ts
+++ src/app/analitica/services/analitica.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {FilterIncidente, ResultadoIncidente, Resultados} from "../model";
+import {FilterIncidente, ResultadoIncidente} from "../model";
 
 let headers = new HttpHeaders({ 'Content-Type': 'application/json' })
 
