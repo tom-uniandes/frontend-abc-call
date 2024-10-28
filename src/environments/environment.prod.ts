6ea0a0a7739017ddf20dbdc330250c5891b44a1f
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-10-27 21:53:13 -0500

    Add connection with gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://gateway-microservice.proyecto-final-cluster:5000'
+  baseUrl: 'http://100.26.224.51:5000'
 
   //For test with docker
   //baseUrl: 'http://localhost:5000'
