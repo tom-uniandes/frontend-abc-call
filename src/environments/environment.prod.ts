857f37b31a7cd86abc2608c413a07c2398b74378
santiagomd11 <your_email@example.com>
2024-11-02 13:49:14 -0500

    Updating gateway base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://100.26.224.51:5000'
+  baseUrl: 'http://proyecto-final-miso-lb-1181535848.us-east-1.elb.amazonaws.com:5000'
 
   //For test with docker
   //baseUrl: 'http://localhost:5000'
