33515e46af38d7aaee0fb89273db0d0d76ae8e7c
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2024-11-16 13:42:33 -0500

    Update test response incidents

--- src/app/incidents/response-incident/response-incident.component.spec.ts
+++ src/app/incidents/response-incident/response-incident.component.spec.ts
@@ -1,63 +1,114 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResponseIncidentComponent } from './response-incident.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ToastrModule } from 'ngx-toastr';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { IncidentsService } from '../incidents.service';
-import { NavbarComponent } from '../../menu/navbar/navbar.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+// Mock Services
+class MockIncidentsService {
+  getIncident = jasmine.createSpy('getIncident').and.returnValue(of({ solved: false, id: '1' }));
+  update_incident_response = jasmine.createSpy('update_incident_response').and.returnValue(of({}));
+}
+
+class MockToastrService {
+  success = jasmine.createSpy('success');
+  error = jasmine.createSpy('error');
+}
+
+class MockActivatedRoute {
+  snapshot = { paramMap: new Map([['id', '1']]) };
+}
+
+class MockRouter {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('ResponseIncidentComponent', () => {
   let component: ResponseIncidentComponent;
   let fixture: ComponentFixture<ResponseIncidentComponent>;
+  let incidentsService: IncidentsService;
+  let toastrService: ToastrService;
+  let activatedRoute: ActivatedRoute;
+  let router: Router;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-        ToastrModule.forRoot(),
-        ReactiveFormsModule,
-        RouterModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatIconModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatOptionModule,
-        MatSelectModule,
-        BrowserAnimationsModule,
-        NoopAnimationsModule
-      ],
-      declarations: [ResponseIncidentComponent,
-        NavbarComponent],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ResponseIncidentComponent],
       providers: [
-        IncidentsService
+        FormBuilder,
+        { provide: IncidentsService, useClass: MockIncidentsService },
+        { provide: ToastrService, useClass: MockToastrService },
+        { provide: ActivatedRoute, useClass: MockActivatedRoute },
+        { provide: Router, useClass: MockRouter }
       ]
-    })
-    .compileComponents();
-  }));
-
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ResponseIncidentComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    incidentsService = TestBed.inject(IncidentsService);
+    toastrService = TestBed.inject(ToastrService);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+
+    fixture.detectChanges(); // Trigger ngOnInit
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the form with default values', () => {
+    expect(component.responseForm).toBeDefined();
+    expect(component.responseForm.controls['response']).toBeDefined();
+    expect(component.responseForm.controls['company'].value).toBeTruthy();
+    expect(component.responseForm.controls['incidentId'].value).toBe('1');
+  });
+
+  it('should load incident details on ngOnInit', () => {
+    component.ngOnInit();
+    expect(incidentsService.getIncident).toHaveBeenCalledWith('1', ''); // empty company from session
+    expect(component.incident).toEqual({ solved: false, id: '1' });
+  });
+
+  it('should show error if the incident is solved', () => {
+    component.ngOnInit();
+    expect(incidentsService.getIncident).toHaveBeenCalledWith('1', '1');
+    expect(component.incident).toEqual({ solved: true, id: '1' });
+    expect(toastrService.error).toHaveBeenCalledWith('El incidente ya se encuentra resuelto');
+    expect(router.navigate).toHaveBeenCalledWith(['/incidents/incident-detail', '1']);
+  });
+
+  it('should submit form and call incidentsService.update_incident_response', () => {
+    component.responseForm.controls['response'].setValue('Response to the incident');
+    component.responseIncident();
+    expect(incidentsService.update_incident_response).toHaveBeenCalledWith({
+      response: 'Response to the incident',
+      company: null, // As returned by sessionStorage
+      incidentId: '1'
+    });
+    expect(toastrService.success).toHaveBeenCalledWith('Respuesta del incidente registrado con éxito');
+  });
+
+  it('should handle error when updating incident response', () => {
+    const errorResponse = new HttpErrorResponse({
+      error: { message: 'Test error' },
+      status: 400,
+      statusText: 'Bad Request'
+    });
+    incidentsService.update_incident_response(throwError(errorResponse));
+
+    component.responseIncident();
+    expect(toastrService.error).toHaveBeenCalledWith('Test error');
+  });
+
+  it('should navigate to incident detail page after success', () => {
+    component.incident = { id: '1' };
+    component.returnViewDetalleIncident();
+    expect(router.navigate).toHaveBeenCalledWith(['/incidents/incident-detail', '1']);
+  });
 });
-
-
-
