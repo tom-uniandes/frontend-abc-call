033062e31a6ccda4465003fdbf8d1d7e95d62d10
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2024-11-16 13:14:35 -0500

    Update test response incidents

--- src/app/incidents/response-incident/response-incident.component.spec.ts
+++ src/app/incidents/response-incident/response-incident.component.spec.ts
@@ -1,17 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import { ResponseIncidentComponent } from './response-incident.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IncidentsService } from '../incidents.service';
+import { NavbarComponent } from '../../menu/navbar/navbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ResponseIncidentComponent', () => {
   let component: ResponseIncidentComponent;
   let fixture: ComponentFixture<ResponseIncidentComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ResponseIncidentComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        ToastrModule.forRoot(),
+        ReactiveFormsModule,
+        RouterModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatOptionModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule
+      ],
+      declarations: [ResponseIncidentComponent,
+        NavbarComponent],
+      providers: [
+        IncidentsService
+      ]
     })
     .compileComponents();
+  }));
 
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ResponseIncidentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,3 +58,6 @@ describe('ResponseIncidentComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+
