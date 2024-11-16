fc45f569400b28d7bcbffdbe6f8da93f7c614043
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2024-11-16 12:22:07 -0500

    Update response incidents

--- src/app/incidents/response-incident/response-incident.component.ts
+++ src/app/incidents/response-incident/response-incident.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Incident} from "../incident";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {IncidentsService} from "../incidents.service";
