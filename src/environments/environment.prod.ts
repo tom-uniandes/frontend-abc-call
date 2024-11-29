58e5003b506c3357f237174d60cc9d933a5824ad
Tomas Octavio Rodriguez Herrera <t.rodriguezh@uniandes.edu.co>
2024-11-29 00:17:04 -0500

    Release 1.0.0 terminada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://proyecto-final-miso-lb-1181535848.us-east-1.elb.amazonaws.com:5000',
+  baseUrl: 'https://api.abc-call-grupo-11.lat:5000',
   publicKey: '088653d6-4ccd-4b07-a3ee-ce4a77abd1ec'
 
   //For test with docker
